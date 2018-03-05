39aa9ea4eafacd7c4402e2a7ded372e8f1525e0e
Alisha <lamichhane.alisha97gmail.com>
2018-03-05 17:17:54 +0545

    css folder embed

--- index.php
+++ index.php
@@ -2,9 +2,17 @@
 <html>
 <head>
 	<title>Practice</title>
+	<link rel="stylesheet" type="text/css" href="css\bootstrap.css">
 </head>
 <body>
 	<h1>My practice project</h1>
+	<h3>Zivah</h3>
+	<ul>
+		<li>Home</li>
+		<li>Our services</li>
+		<li>Contact Us</li>
+	</ul>
+
 
 </body>
 </html>
